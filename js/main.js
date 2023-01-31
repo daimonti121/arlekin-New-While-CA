0928b16e8f9092e995935642b8b89068da8b1439
Yushenko Dima <damios.1812@gmail.com>
2023-01-31 11:07:11 +0200

    30.01.2023-3

--- js/main.js
+++ js/main.js
@@ -70,3 +70,12 @@ window.onload = function () {
 
 
 };
+
+function $_GET(e,t){return!!(t=t.match(new RegExp(e+"=([^&=]+)")))&&t[1]}
+var getStag = $_GET("stag", window.location.href);
+var stag = "?stag=" + getStag;
+console.log("STAG -" + stag);
+
+$('.popup__win-btn').click(function(){     
+    window.location.href = 'https://arlekincasino.net/en-CA/users/sign_up' + stag;
+})
